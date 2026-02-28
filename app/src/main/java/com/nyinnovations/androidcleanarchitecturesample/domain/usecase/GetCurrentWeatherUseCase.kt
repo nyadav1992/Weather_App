24b723c8a047d7bb514d09aeb55d95f2635e25d0
Neeraj Yadav <imnirajyadav@gmail.com>
2026-02-28 23:39:28 +0530

    Remove unused use case classes

--- app/src/main/java/com/nyinnovations/androidcleanarchitecturesample/domain/usecase/GetCurrentWeatherUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.nyinnovations.androidcleanarchitecturesample.domain.usecase
-
-import com.nyinnovations.androidcleanarchitecturesample.domain.repository.WeatherRepository
-
-class GetCurrentWeatherUseCase(private val repository: WeatherRepository) {
-    operator fun invoke(cityName: String) = repository.getCurrentWeather(cityName)
-}
-
--- app/src/main/java/com/nyinnovations/androidcleanarchitecturesample/domain/usecase/GetForecastUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.nyinnovations.androidcleanarchitecturesample.domain.usecase
-
-import com.nyinnovations.androidcleanarchitecturesample.domain.repository.WeatherRepository
-
-class GetForecastUseCase(private val repository: WeatherRepository) {
-    operator fun invoke(cityName: String) = repository.getForecast(cityName)
-}
-
